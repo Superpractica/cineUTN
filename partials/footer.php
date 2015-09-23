6292bc6a7dc993e84fbf56d305ddc289cd7b9480
nikobm90 <nikobm90@hotmail.com>
2015-09-23 16:28:46 -0300

    PHP
    Realice los mismos cambios en las pantallas (precios, formatos, cines)

--- partials/footer.php
+++ partials/footer.php
@@ -1,5 +1,6 @@
             <div class="row footer">
-                <span>Nicolas Baduel Copyright © - UTN Pogramación Avanzada II - 2015</span>
-            </div>      
-    </body>
+			<span> Baduel, Nicolas - Carratelli, Andres - García Massaferro, Patricio - UTN Practica Supervisada - Copyright © 2015</span>
+		</div> 
+	</div>
+	</body>
 </html>
